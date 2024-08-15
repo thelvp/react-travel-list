3d2d499243c747f95571bcfff4e85d8c3070fcd7
Loes van Puijenbroek <loesvanpuijenbroek@gmail.com>
2024-08-15 16:43:21 +0200

    packingList setup

--- src/App.js
+++ src/App.js
@@ -1,3 +1,10 @@
+const initialItems = [
+  {id: 1, description: 'Passports', quantity: 2, packed: true},
+  {id: 2, description: 'Socks', quantity: 12, packed: false},
+  {id: 3, description: 'Charger', quantity: 1, packed: false},
+  {id: 4, description: 'Book', quantity: 1, packed: true},
+];
+
 export default function App() {
   return (
     <div className='app'>
@@ -10,7 +17,7 @@ export default function App() {
 }
 
 function Logo() {
-  return <h1>Far Away 🏝️</h1>;
+  return <h1>🌺 Far Away 🏝️</h1>;
 }
 
 function Form() {
@@ -21,13 +28,32 @@ function Form() {
   );
 }
 function PackingList() {
-  return <div className='list'> LIST </div>;
+  return (
+    <div className='list'>
+      <ul>
+        {initialItems.map((item) => (
+          <Item item={item} />
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+function Item({item}) {
+  return (
+    <li>
+      <span style={item.packed ? {textDecoration: 'line-through'} : {}}>
+        {item.quantity} {item.description}
+      </span>
+      <button>❌</button>
+    </li>
+  );
 }
 
 function Stats() {
   return (
     <footer className='stats'>
-      <em>You have X items on your list and you already packed X%!</em>
+      <em>🧳 You have X items on your list and you already packed X%!</em>
     </footer>
   );
 }
