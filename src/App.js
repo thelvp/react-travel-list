6f0db55c7e68c522072d360983d0744e9ff06739
Loes van Puijenbroek <loesvanpuijenbroek@gmail.com>
2024-08-15 16:31:18 +0200

    standard setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
-function App() {
+export default function App() {
   return (
-    <div className='App'>
-      <p>Yoyo</p>
+    <div className='app'>
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
     </div>
   );
 }
 
-export default App;
+function Logo() {
+  return <h1>Far Away 🏝️</h1>;
+}
+
+function Form() {
+  return (
+    <div className='add-form'>
+      <h3>What do you need for your trip?</h3>
+    </div>
+  );
+}
+function PackingList() {
+  return <div className='list'> LIST </div>;
+}
+
+function Stats() {
+  return (
+    <footer className='stats'>
+      <em>You have X items on your list and you already packed X%!</em>
+    </footer>
+  );
+}
